e4eeb03adf0122be11604edf5d61c418c5a8d991
Christopher Isaac Rivera Valderrama <christoferriveravalderrama@gmail.com>
2026-07-26 00:38:59 -0600

    FIX AND STYLE: Se creo la carpeta theme donde establecemos el color y el cuerpo de la aplicacion y en app_colors establecimos la paleta de colores

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,19 @@
+// En esta clase se definen los colores de la aplicación
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xff7A1C2E);
+  static const Color secondary = Color(0xffFBEAEC);
+  static const Color accentColor = Color(0xffA30036);
+  static const Color textColor = Color.fromARGB(255, 0, 0, 0);
+  static const Color successColor = Color.fromARGB(255, 0, 200, 83);
+  static const Color errorColor = Color.fromARGB(255, 213, 0, 0);
+  static const Color warningColor = Color.fromARGB(255, 255, 165, 0);
+  static const Color infoColor = Color.fromARGB(255, 33, 150, 243);
+  static const Color backgroundColor = Color.fromARGB(255, 255, 255, 255);
+  static const Color cardColor = Color(0xffFFFFFF);
+  static const Color buttonColor = Color(0xff7A1C2E);
+  static const Color buttonTextColor = Color(0xffFFFFFF);
+  static const Color buttonDisabledColor = Color(0xff9E9E9E);
+  static const Color buttonDisabledTextColor = Color(0xffFFFFFF);
+}
---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,1 @@
+// En esta clase se define el tema de la aplicación
